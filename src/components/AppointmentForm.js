01f63c3a4e165eb4c0ae8c7a438c857526edb178
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-13 16:16:03 -0500

    [+] Component > AppointmentForm: State Error & Validacion

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -11,6 +11,9 @@ const AppointmentForm = () => {
         symptoms: ''
     });
 
+    /** State: Error */
+    const [ stateError, setError ] = useState( false );
+
     /** Manejador de Cambios el State: Cita (Cuando el usuario escribe en un campo del formulario) */
     const handleChange = ( event ) => {
         console.log( 'Escribiendo en', event .target .name );
@@ -25,10 +28,43 @@ const AppointmentForm = () => {
     /** Extraer los datos */
     const { petName, ownerName, medicalDepartureDate, medicalDepartureTime, symptoms } = stateAppointment;
 
+    /** Evento que solicita la cita al hacer click en el formulario */
+    const submitAppointment = ( event ) => {
+        event .preventDefault();        // Evita el envio por el QueryString por el metodo GET
+
+        /** Valida campos del formulario*/
+        if( 
+            petName .trim() === '' || 
+            ownerName .trim() === '' ||
+            medicalDepartureDate .trim() === '' ||
+            medicalDepartureTime .trim() === '' ||
+            symptoms .trim() === ''
+        ) {
+            console .error( 'Todos los campos son obligatorios.' );
+            setError( true );       // Actualiza el State: Error
+            return;
+        }
+        
+        console.log( 'Cita', stateAppointment );
+        setError( false );       // Actualiza el State: Error
+
+        /** Asigna un ID */
+
+        /** Crea la cita */
+
+        /** Resetea campos del formulario */
+    }
+
     return (
         <Fragment>
             <h3>Solicitud de cita</h3>
-            <form id="appointment-form">
+            { stateError 
+                ?   <p className="alert-error">Todos los campos son obligatorios.</p>
+                :   null            }
+            <form 
+                id="appointment-form"
+                onSubmit={ submitAppointment }
+            >
                 <label>Nombre Mascota</label>
                 <input 
                     name="petName"
@@ -72,7 +108,7 @@ const AppointmentForm = () => {
                     value={ symptoms }
                 ></textarea>
                 <button
-                    type="button"
+                    type="submit"
                     className="u-full-width button-primary"
                 >Solicitar</button>
             </form>

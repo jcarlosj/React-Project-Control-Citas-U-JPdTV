52d6378d54a113191ecd1f3b65a429419b261132
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-13 13:45:21 -0500

    [+] Component > AppointmentForm: Establece el State

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -1,6 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const AppointmentForm = () => {
+
+    /** State: Cita */
+    const [ stateAppointment, setAppointment ] = useState({
+        petName: '',
+        ownerName: '',
+        medicalDepartureDate: '',
+        medicalDepartureTime: '',
+        symptoms: ''
+    });
+
+    /** Manejador de Cambios el State: Cita (Cuando el usuario escribe en un campo del formulario) */
+    const handleChange = ( event ) => {
+        console.log( 'Escribiendo en', event .target .name );
+    }
+
     return (
         <Fragment>
             <h3>Solicitud de cita</h3>
@@ -11,6 +26,7 @@ const AppointmentForm = () => {
                     type="text"
                     placeholder="Ej: Rex"
                     className="u-full-width"
+                    onChange={ handleChange }
                 />
                 <label>Nombre Propietario</label>
                 <input 
@@ -18,24 +34,28 @@ const AppointmentForm = () => {
                     type="text"
                     placeholder="Ej: Juan Carlos Jiménez Gutiérrez"
                     className="u-full-width"
+                    onChange={ handleChange }
                 />
                 <label>Fecha de Alta</label>
                 <input 
                     name="medicalDepartureDate"
                     type="date"
                     className="u-full-width"
+                    onChange={ handleChange }
                 />
                 <label>Hora de Alta</label>
                 <input 
                     name="medicalDepartureTime"
                     type="time"
                     className="u-full-width"
+                    onChange={ handleChange }
                 />
                 <label>Síntomas</label>
                 <textarea
                     name="symptoms"
                     className="u-full-width"
                     placeholder="Haga una descripción detallada de toda la sintomatología de su mascota"
+                    onChange={ handleChange }
                 ></textarea>
                 <button
                     type="button"

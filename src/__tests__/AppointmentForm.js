cc1f060a6e2c4b8f45f3992c5545314e2a042867
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-18 11:50:43 -0500

    [+] Test > AppointmentForm: Define prueba de componente cambiando la sintaxis test por it en la deficion

---
+++ src/__tests__/AppointmentForm.js
@@ -0,0 +1,8 @@
+it( '<AppointmentForm /> Carga', () => {});
+
+it( '<AppointmentForm /> Validacion', () => {});
+
+/** PASS  src/__tests__/AppointmentForm.js
+  √ <AppointmentForm /> Carga (2ms)
+  √ <AppointmentForm /> Validacion
+*/

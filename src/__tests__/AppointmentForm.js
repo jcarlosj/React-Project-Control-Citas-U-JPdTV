7d89a616aed6d57c65fd8be69d276d5880fdd34d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-18 13:03:46 -0500

    [$] Test > AppointmentForm: Define test -> Debe obtener elemento con el texto 'Solicitud de cita' (Nueva Sintaxis)

--- src/__tests__/AppointmentForm.js
+++ src/__tests__/AppointmentForm.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';       //  Para dar soporte a toBeInTheDocument()
 
 /** Componente a Testear */
 import AppointmentForm from '../components/AppointmentForm';
 
-test('<AppointmentForm /> ', () => {
+test('<AppointmentForm /> Debe obtener elemento con el texto "Solicitud de cita" ', () => {
     // const wrapper = render( <AppointmentForm /> );
     // wrapper .debug();
 
-    const { getByText } = render( <AppointmentForm /> );    //  Monta Componente
+    render( <AppointmentForm /> );    //  Monta Componente
 
     /** Assert */
     expect( 
-        getByText( 'Solicitud de cita' )    //  Obtiene el elemento con el texto indicado
-    ) .toBeInTheDocument();                 //  Verifica si el elemento esta en el DOM
+        screen .getByText( 'Solicitud de cita' )    //  Obtiene el elemento con el texto indicado
+    ) .toBeInTheDocument();                         //  Verifica si el elemento esta en el DOM
 });

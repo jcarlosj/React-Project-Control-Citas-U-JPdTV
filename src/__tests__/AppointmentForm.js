cb35258b0d9c4e76e67b3d1a6105c63e3d8fd21d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-19 10:15:15 -0500

    [+] Test > AppointmentForm: 1. Implementa montaje de componente con su funcionalidad (Usando Jest Spy Function) 2. Desmonta el Componente en después de la ejecución de cada te st3. Reactiva el PropType de la función como requerida

--- src/__tests__/AppointmentForm.js
+++ src/__tests__/AppointmentForm.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';       //  Para dar soporte a toBeInTheDocument()
 
 /** Componente a Testear */
 import AppointmentForm from '../components/AppointmentForm';
 
+/** Jest Spy Function */
+const addAppointment = jest .fn();
+
+/** Funciones Jest  */
+afterEach( cleanup );       //   Desmonta árboles de React que se montaron con render despues de la finalización de cada test      
+
 test('<AppointmentForm /> Verifica etiqueta y contenido de encabezado y boton enviar', () => {
     // const wrapper = render( <AppointmentForm /> );
     // wrapper .debug();
 
-    render( <AppointmentForm /> );    //  Monta Componente
+    /** Monta el Componente con su funcionalidad asociada */
+    render( 
+        <AppointmentForm 
+            addAppointment={ addAppointment }       // Usamos Props para pasar la funcion al componente y establecer una comunicación con el padre 'App'
+        />
+    );    
 
     const
         textTitle = screen .getByText( 'Solicitud de cita' ),
@@ -25,5 +36,16 @@ test('<AppointmentForm /> Verifica etiqueta y contenido de encabezado y boton en
     /** Submit Button */
     expect( btnSubmit .tagName ) .toBe( 'BUTTON' );             //  Verifica la etiqueta del elemento sea un BUTTON
     expect( btnSubmit .textContent ). toBe( 'Solicitar' );      //  Verifica la etiqueta posea el texto esperado
+
+});
+
+test( '<AppointmentForm /> Valida el formulario', () => {
+    /** Monta el Componente con su funcionalidad asociada */
+    render( 
+        <AppointmentForm 
+            addAppointment={ addAppointment }       // Usamos Props para pasar la funcion al componente y establecer una comunicación con el padre 'App'
+        />
+    ); 
+
 
 });
